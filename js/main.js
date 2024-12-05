104314379dd561f0b953409c4e30bba80c6697a5
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2024-12-05 14:26:45 +0200

    check if site (name||url) in localstorage or not and add site of one of them not in it

--- js/main.js
+++ js/main.js
@@ -2,28 +2,69 @@ var siteName = document.getElementById("site-name");
 var siteUrl = document.getElementById("websiteUrl");
 var inputnamevalid = document.getElementById("site-name");
 var inputurlvalid = document.getElementById("websiteUrl");
-
+//
 var allSites = [];
+
+
 var found = localStorage.getItem("websites");
-found != undefined ? (allSites = JSON.parse(found)) : (allSites = []);
-displaySite();
-
-function addSite(allsites) {
-if (validationurlSite() && validationNameSite()) {
-    onesite = {
-    sitename: siteName.value,
-    siteurl: siteUrl.value,
-    };
-    allSites.push(onesite);
-    localStorage.setItem("websites", JSON.stringify(allSites));
-    displaySite();
-    console.log(allSites);
-    clearinput();
+if (found != undefined) {
+  allSites = JSON.parse(found);
+  displaySite();
+} else {
+  allSites = [];
 }
-inputurlvalid.classList.remove("is-valid");
-inputnamevalid.classList.remove("is-valid");
-}
+//ternary operator
+// var found = localStorage.getItem("websites");
+// found != undefined ? (allSites = JSON.parse(found)) : (allSites = []);
+// displaySite();
 
+function testisNamefound() {
+  for (var i = 0; i < allSites.length; i++) {
+    //test statment
+    console.log(allSites[i].sitename == siteName.value);
+    console.log(allSites[i].siteurl == siteUrl.value);
+    if (
+      allSites[i].sitename == siteName.value ||
+      allSites[i].siteurl == siteUrl.value
+    ) {
+      //inputnamevalid.classList.add("is-invalid");
+      // inputurlvalid.classList.add("is-invalid");
+      console.log("nnn")
+      return true;
+    } 
+  
+  }
+}
+//===============adding site=====================
+function addSite() {
+  
+  if (validationNameSite() && validationurlSite()) {
+    if (testisNamefound() === true) {
+      ///// حاولت كتير والله  بس مش عاف فين المشكله بص كده في الكونسول ///// مضيفش 
+      // ما ضيفتش اهو
+      console.log("fffff");
+    //  mess.classList.remove("d-none");
+      return mess1.classList.remove("d-none") + mess.classList.remove("d-none");
+    
+    }
+  else  {
+      var onesite = {
+        sitename: siteName.value,
+        siteurl: siteUrl.value,
+      };
+      allSites.push(onesite);
+      localStorage.setItem("websites", JSON.stringify(allSites));
+      displaySite();
+      console.log(allSites);
+      clearinput();
+     }
+  }
+ 
+  }
+
+//========  enddd add =========
+
+//======function to display the name of site and url==========
 function displaySite() {
   var cartona = "";
   for (let i = 0; i < allSites.length; i++) {
@@ -53,10 +94,14 @@ function displaySite() {
   document.getElementById("newWebsite").innerHTML = cartona;
 }
 
+//==========function to clear the input after you add site===========
 function clearinput() {
   siteName.value = null;
   siteUrl.value = null;
+    inputurlvalid.classList.remove("is-valid");
+    inputnamevalid.classList.remove("is-valid");
 }
+//==========function to delete  one site that you want to delete ===========
 
 function deleteElementy(index) {
   allSites.splice(index, 1);
@@ -64,43 +109,12 @@ function deleteElementy(index) {
   displaySite();
 }
 
-// function validateUrlsite(){
-//    var urlReegex =
-//   /^(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?$/i;
-// //   var urlReegex = /^https\:\/\/www\.[\w\W]{0,20}\.com[\w\W]{0,}$/i;
-//  return urlReegex.test(siteUrl.value);
-// }
-
-function validationurlSite() {
-  // var urlRegex = /^https\:\/\/www\.[\w\W]{0,20}\.com[\w\W]{0,}$/i;
-  var urlRegex =
-    /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$/g;
-
-  var mess = document.getElementById("mess");
-  var urle = siteUrl.value;
-  if (urlRegex.test(urle)) {
-    mess.classList.add("d-none");
-
-    inputurlvalid.classList.add("is-valid");
-    inputurlvalid.classList.remove("is-invalid");
-
-    return true;
-  } else {
-    mess.classList.remove("d-none");
-
-    inputurlvalid.classList.remove("is-valid");
-    inputurlvalid.classList.add("is-invalid");
-
-    return false;
-  }
-}
-
+//validation when you write in input about Site Name
 function validationNameSite() {
   //local
-  var sitenameRegex = /^[a-zA-Z ][a-zA-Z1-9 ]{2,19}$/;
+  var sitenameRegex = /^[a-zA-Z ][a-zA-Z1-9 ]{2,}$/;
   var text = siteName.value;
   var mess1 = document.getElementById("mess1");
-
   if (sitenameRegex.test(text)) {
     mess1.classList.add("d-none");
     inputnamevalid.classList.add("is-valid");
@@ -110,21 +124,26 @@ function validationNameSite() {
     mess1.classList.remove("d-none");
     inputnamevalid.classList.add("is-invalid");
     inputnamevalid.classList.remove("is-valid");
-
     return false;
   }
 }
 
-///  ده هجس متبصش عليه
-// function checkname() {
-
-// if(
-//  JSON.parse(localStorage.getItem("websites")).include(inputnamevalid )
-// ) {
-//     mess1.classList.add("d-none");
-//     return false
-//     }
-// else {
-//     return true
-//     }
-// }
+//validation when you write in input about Site URL
+function validationurlSite() {
+  // var urlRegex = /^https\:\/\/www\.[\w\W]{0,20}\.com[\w\W]{0,}$/i;
+  var urlRegex =/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$/g;
+  var mess = document.getElementById("mess");
+  var urle = siteUrl.value;
+  if (urlRegex.test(urle)) {
+      mess.classList.add("d-none");
+      inputurlvalid.classList.add("is-valid");
+      inputurlvalid.classList.remove("is-invalid");
+    return true;
+  }
+  else {
+    mess.classList.remove("d-none");
+    inputurlvalid.classList.remove("is-valid");
+    inputurlvalid.classList.add("is-invalid");
+    return false;
+  }
+}
